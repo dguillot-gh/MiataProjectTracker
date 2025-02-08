3c8d87c39e748039a531864214262310cc6f5b5c
dan <dguillot90@gmail.com>
2025-02-08 16:56:47 -0600

    changes to add validations for data fields

--- Models/Parts.cs
+++ Models/Parts.cs
@@ -7,10 +7,17 @@ namespace MiataProjectTracker.Models;
 public class Parts
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(60, MinimumLength = 3)]
     public string? PartName { get; set; }
     public string? PartNumber { get; set; }
+
+    [Required]
+    [StringLength(60, MinimumLength = 3)]
     public string? PartStatus { get; set; }
 
+    [Range(0, 1000)]
     [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal? PartCost { get; set; 
