be3db5c95cf92f7bb858459fe9f58358ab72f41e
dan <dguillot90@gmail.com>
2025-02-08 17:41:56 -0600

    trying to get search to work

--- Migrations/20250208225736_NewDataAnnotations.Designer.cs
+++ Migrations/20250208225736_NewDataAnnotations.Designer.cs
@@ -2,6 +2,7 @@
 using MiataProjectTracker.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MiataProjectTracker.Migrations
 {
     [DbContext(typeof(MiataProjectTrackerContext))]
-    partial class MiataProjectTrackerContextModelSnapshot : ModelSnapshot
+    [Migration("20250208225736_NewDataAnnotations")]
+    partial class NewDataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
