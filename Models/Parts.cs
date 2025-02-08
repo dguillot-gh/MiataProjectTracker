fad6d5b127eb2eeacd0556cefd0d919d95361955
dan <dguillot90@gmail.com>
2025-02-08 14:47:56 -0600

    attempting to use sqllite

---
+++ Models/Parts.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace MiataProjectTracker.Models;
+
+public class Parts
+{
+    public int Id { get; set; }
+    public string? PartName { get; set; }
+    public string? PartNumber { get; set; }
+    public string? PartStatus { get; set; }
+
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal? PartCost { get; set; 
+    }
+}
