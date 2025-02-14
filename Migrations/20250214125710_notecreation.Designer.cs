eed2920853e1a6834a689ad8aaf264bd3f1a15ac
dan <dguillot90@gmail.com>
2025-02-14 07:17:05 -0600

    made a notes style build log

--- Migrations/20250214125710_notecreation.Designer.cs
+++ Migrations/20250214125710_notecreation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MiataProjectTracker.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MiataProjectTracker.Migrations
 {
     [DbContext(typeof(MiataProjectTrackerContext))]
-    partial class MiataProjectTrackerContextModelSnapshot : ModelSnapshot
+    [Migration("20250214125710_notecreation")]
+    partial class notecreation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.2");
