4a816a38d7736a5316128ba82f7d1c1c0235cc8b
dan <dguillot90@gmail.com>
2025-02-14 18:40:48 -0600

    adding tag validation system

---
+++ Models/BuildLog.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MiataProjectTracker.Models;
+
+public class BuildLog
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string? Title { get; set; }
+
+    [Required]
+    public string? Summary { get; set; }
+    [Required]
+    public string? Tag { get; set; }
+    public DateOnly? Date { get; set; }
+}
