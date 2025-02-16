98ec1cf0d01996549d88b92835fa6d7ffcc882ec
dan <dguillot90@gmail.com>
2025-02-15 18:38:05 -0600

    start of price tracker tool

---
+++ Migrations/20250216002942_sqlite_migration_596.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiataProjectTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class sqlite_migration_596 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KanbanTask");
+
+            migrationBuilder.CreateTable(
+                name: "PriceComparison",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PartName = table.Column<string>(type: "TEXT", nullable: false),
+                    NewPrice = table.Column<decimal>(type: "TEXT", nullable: false),
+                    ActualPaidPrice = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Source = table.Column<string>(type: "TEXT", nullable: true),
+                    PurchaseDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Notes = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PriceComparison", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PriceComparison");
+
+            migrationBuilder.CreateTable(
+                name: "KanbanTask",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Priority = table.Column<int>(type: "INTEGER", nullable: true),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KanbanTask", x => x.Id);
+                });
+        }
+    }
+}
